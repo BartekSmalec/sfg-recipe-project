b0f11262b846847922414080dfc4aa3cc3a44ce4
bartek <smalecbartlomiej@gmail.com>
2023-01-01 18:23:42 +0100

    Added Category and Difficulty entities

--- src/main/java/com/barteksmalec/sfgrecipeproject/model/Recipe.java
+++ src/main/java/com/barteksmalec/sfgrecipeproject/model/Recipe.java
@@ -16,10 +16,16 @@ public class Recipe {
     private String source;
     private String url;
     private String directions;
-    //todo add
-    //private Difficulty difficulty
     @Lob
     private Byte[] image;
+
+    @Enumerated(value = EnumType.STRING)
+    private Difficulty difficulty;
+
+    @ManyToMany
+    @JoinTable(name = "recipe_category", joinColumns = @JoinColumn(name = "recipe_id"), inverseJoinColumns = @JoinColumn(name = "category_id"))
+    private Set<Category> categories;
+
     @OneToOne(cascade = CascadeType.ALL)
     private Notes notes;
 
@@ -112,5 +118,21 @@ public class Recipe {
 
     public void setIngredients(Set<Ingredient> ingredients) {
         this.ingredients = ingredients;
+    }
+
+    public Difficulty getDifficulty() {
+        return difficulty;
+    }
+
+    public void setDifficulty(Difficulty difficulty) {
+        this.difficulty = difficulty;
+    }
+
+    public Set<Category> getCategories() {
+        return categories;
+    }
+
+    public void setCategories(Set<Category> categories) {
+        this.categories = categories;
     }
 }
