bac6a6e1f7fa1592bdab4acf31cc9e1125144cb7
bartek <smalecbartlomiej@gmail.com>
2023-01-01 19:03:30 +0100

    Added data.sql init file

--- src/main/java/com/barteksmalec/sfgrecipeproject/model/UnitOfMeasure.java
+++ src/main/java/com/barteksmalec/sfgrecipeproject/model/UnitOfMeasure.java
@@ -11,6 +11,7 @@ public class UnitOfMeasure {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String uom;
+    private String description;
 
     public Long getId() {
         return id;
@@ -26,5 +27,13 @@ public class UnitOfMeasure {
 
     public void setUom(String uom) {
         this.uom = uom;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
     }
 }
