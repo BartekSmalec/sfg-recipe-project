76834a7b6ca28fda20adf5aa618414461eb58cce
bartek <smalecbartlomiej@gmail.com>
2023-01-01 14:48:28 +0100

    completing assigment

--- src/main/java/com/barteksmalec/sfgrecipeproject/controllers/IndexController.java
+++ src/main/java/com/barteksmalec/sfgrecipeproject/controllers/IndexController.java
@@ -6,8 +6,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class IndexController {
 
-    @RequestMapping({"","/","/index"})
-    public String getIndexPage(){
+    @RequestMapping({"", "/", "/index"})
+    public String getIndexPage() {
         System.out.println("Test dev tool 111111");
         return "index";
     }
