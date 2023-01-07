db201ddfee93f613dad8c2fe7fc06a60464c6c06
bartek <smalecbartlomiej@gmail.com>
2023-01-07 12:40:48 +0100

    Added exception handler + error template

--- src/main/java/com/barteksmalec/sfgrecipeproject/controllers/RecipeController.java
+++ src/main/java/com/barteksmalec/sfgrecipeproject/controllers/RecipeController.java
@@ -1,14 +1,14 @@
 package com.barteksmalec.sfgrecipeproject.controllers;
 
 import com.barteksmalec.sfgrecipeproject.commands.RecipeCommand;
+import com.barteksmalec.sfgrecipeproject.exceptions.NotFoundException;
 import com.barteksmalec.sfgrecipeproject.services.RecipeService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
 
 @Slf4j
 @Controller
@@ -49,5 +49,14 @@ public class RecipeController {
         log.debug("Deleting id: " + id);
         recipeService.deletById(Long.valueOf(id));
         return "redirect:/";
+    }
+
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    @ExceptionHandler(NotFoundException.class)
+    public ModelAndView handleNotFound(){
+        log.error("Handling not found exception");
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("404error");
+        return modelAndView;
     }
 }
